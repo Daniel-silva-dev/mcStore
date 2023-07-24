8678452b914db2c4e7347741ac7cb0b30ece4393
Daniel silva <danielsilvadev7@gmail.com>
2023-07-24 20:52:30 -0300

    update day thwo

---
+++ scripts/slide.js
@@ -0,0 +1,12 @@
+const img = document.getElementById("img");
+const imgs = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+setInterval(()=>{
+  idx++;
+  if(idx > img.length -1){
+    idx = 0;
+  }
+  imgs.style.transform = `translateX(${-idx * 500}px)`
+},1800);
