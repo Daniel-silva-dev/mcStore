ca9af21d3a1b0fd42876ff7103e21c85a9fa82f8
Daniel silva <danielsilvadev7@gmail.com>
2023-07-31 15:23:15 -0300

    Update playModaMasculina.js

--- scripts/playModaMasculina.js
+++ scripts/playModaMasculina.js
@@ -1,11 +1,12 @@
 import {modaMasculina} from "../produtos/moda/modaMasculina";
 
 
+console.log(modaMasculina.name)
+
 function inicializarLoja() {
   var containerProdutos = document.querySelector('.modaMasculina');
   modaMasculina.map((val) => {
     containerProdutos.innerHTML += `
-    
     <div class="produtos-single">
     <img src="` + val.img + `" class="produtoImg">
     <p>` + val.name + `</p>
