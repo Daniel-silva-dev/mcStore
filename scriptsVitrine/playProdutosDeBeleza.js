655856739287f37df632c4e2a0f58b1f3b774eec
Daniel silva <danielsilvadev7@gmail.com>
2023-08-14 20:20:45 -0300

    weeeee

--- scriptsVitrine/playProdutosDeBeleza.js
+++ scriptsVitrine/playProdutosDeBeleza.js
@@ -1,19 +1,20 @@
 import {modaMasculina} from "../produtos/moda/modaMasculina";
 
+var index = 0;
+
 function inicializarLoja() {
   var containerProdutos = document.querySelector('.container04');
   modaMasculina.map((val) => {
-    containerProdutos.innerHTML += `
-    
+    if(index <= 3){
+    containerProdutos.innerHTML += ` 
     <div class="produtos-single">
     <img src="` + val.img + `">
     <p class='buy'> R$` + val.price + `</p>
     <p>` + val.name + `</p>
     </div>
-    
     `;
-
+    index++
+  };
   })
-
-};
+}
 inicializarLoja();
